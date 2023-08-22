d699d172bdd3f2c8891fc8e3a33b8afabd570036
Thato Chitja <t.chitja369@gmail.com>
2023-08-22 01:42:52 -0700

    Fix: Correct standard

--- _printf.c
+++ _printf.c
@@ -1,13 +1,23 @@
 #include "main.h"
+
+/**
+ * _printf - printf function recreation
+ * format: string input
+ * Return: number of characters printed
+ */
+
 int _printf(const char *format, ...)
 {
 	int chara_print = 0;
 	va_list list_of_args;
+
 	if (format == NULL)
 	{
 		return (-1);
 	}
+
 	va_start(list_of_args, format);
+
 	while (*format)
 	{
 		if (*format != '%')
@@ -20,6 +30,7 @@ int _printf(const char *format, ...)
 			format++;
 			if (*format == '\0')
 				break;
+
 			if (*format == '%')
 			{
 				write(1, format, 1);
@@ -28,6 +39,7 @@ int _printf(const char *format, ...)
 			else if (*format == 'c')
 			{
 				char c = va_arg(list_of_args, int);
+
 				write(1, &c, 1);
 				chara_print++;
 			}
@@ -35,6 +47,7 @@ int _printf(const char *format, ...)
 			{
 				char *str = va_arg(list_of_args, char*);
 				int str_len = strlen(str);
+
 				write(1, str, str_len);
 				chara_print += str_len;
 			}
@@ -43,6 +56,7 @@ int _printf(const char *format, ...)
 				int num = va_arg(list_of_args, int);
 				char buffer[20];
 				int len = sprintf(buffer, "%d", num);
+
 				write(1, buffer, len);
 				chara_print += len;
 			}
