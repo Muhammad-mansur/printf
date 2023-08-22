27c2b8daab525e36d7580e16718d33749d8ccb8c
Muhammad-mansur <muhammadmansur737@gmail.com>
2023-08-22 01:29:18 -0700

    Add task 1

--- _printf.c
+++ _printf.c
@@ -1,32 +1,13 @@
 #include "main.h"
-
-/**
- * _printf - printf function recreation
- * format: string inout
- * Return: number of characters printed
- */
-
-/* Function prototypes for specifier handlers */
-void handle_char(va_list args);
-void handle_string(va_list args);
-void handle_decimal(va_list args);
-void handle_binary(va_list args);
-void handle_unsigned(va_list args);
-void handle_octal(va_list args);
-void handle_hexadecimal(va_list args, int uppercase);
-
 int _printf(const char *format, ...)
 {
 	int chara_print = 0;
 	va_list list_of_args;
-
 	if (format == NULL)
 	{
 		return (-1);
 	}
-
 	va_start(list_of_args, format);
-
 	while (*format)
 	{
 		if (*format != '%')
@@ -39,7 +20,6 @@ int _printf(const char *format, ...)
 			format++;
 			if (*format == '\0')
 				break;
-
 			if (*format == '%')
 			{
 				write(1, format, 1);
@@ -47,32 +27,24 @@ int _printf(const char *format, ...)
 			}
 			else if (*format == 'c')
 			{
-				handle_char(list_of_args);
+				char c = va_arg(list_of_args, int);
+				write(1, &c, 1);
+				chara_print++;
 			}
 			else if (*format == 's')
 			{
-				handle_string(list_of_args);
+				char *str = va_arg(list_of_args, char*);
+				int str_len = strlen(str);
+				write(1, str, str_len);
+				chara_print += str_len;
 			}
 			else if (*format == 'd' || *format == 'i')
 			{
-				handle_decimal(list_of_args);
-			}
-			else if (*format == 'u')
-			{
-				handle_unsigned(list_of_args);
-			}
-			else if (*format == 'o')
-			{
-				handle_octal(list_of_args);
-			}
-			else if (*format == 'x' || *format == 'X')
-			{
-				int uppercase = (*format == 'X');
-				handle_hexadecimal(list_of_args, uppercase);
-			}
-			else if (*format == 'b')
-			{
-				handle_binary(list_of_args);
+				int num = va_arg(list_of_args, int);
+				char buffer[20];
+				int len = sprintf(buffer, "%d", num);
+				write(1, buffer, len);
+				chara_print += len;
 			}
 			else
 			{
@@ -84,84 +56,4 @@ int _printf(const char *format, ...)
 	}
 	va_end(list_of_args);
 	return (chara_print);
-}
-
-/* Implementation of specifier handler functions */
-void handle_char(va_list args)
-{
-	char c = va_arg(args, int);
-	write(1, &c, 1);
-}
-
-void handle_string(va_list args)
-{
-	char *str = va_arg(args, char*);
-	int str_len = strlen(str);
-	write(1, str, str_len);
-}
-
-void handle_decimal(va_list args)
-{
-	int num = va_arg(args, int);
-	char buffer[20];
-	int len = sprintf(buffer, "%d", num);
-	write(1, buffer, len);
-}
-
-void handle_binary(va_list args)
-{
-	unsigned int num = va_arg(args, unsigned int);
-	char buffer[33];
-	int index;
-
-	int num_bits = 0;
-	unsigned int temp = num;
-
-	while (temp)
-	{
-		num_bits++;
-		temp >>= 1;
-	}
-
-	if (num_bits == 0)
-	{
-		buffer[0] = '0';
-		num_bits = 1;
-	}
-
-	buffer[num_bits] = '\0';
-
-	for (index = num_bits - 1; index >= 0; index--)
-	{
-		buffer[num_bits - index - 1] = (num >> index) & 1 ? '1' : '0';
-	}
-
-	write(1, buffer, num_bits);
-}
-
-void handle_unsigned(va_list args)
-{
-	unsigned int num = va_arg(args, unsigned int);
-	char buffer[20];
-	int len = sprintf(buffer, "%u", num);
-	write(1, buffer, len);
-}
-
-void handle_octal(va_list args)
-{
-	unsigned int num = va_arg(args, unsigned int);
-	char buffer[20];
-	int len = sprintf(buffer, "%o", num);
-	write(1, buffer, len);
-}
-
-void handle_hexadecimal(va_list args, int uppercase)
-{
-	unsigned int num = va_arg(args, unsigned int);
-	char buffer[20];
-
-	char *formatSpecifier = (uppercase ? "%X" : "%x");
-
-	int len = sprintf(buffer, formatSpecifier, num);
-	write(1, buffer, len);
 }
